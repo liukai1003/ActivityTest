25a8e113aaf00c2f1029c41e81086b2ac7cc98d5
zeus <liukai15101520137@gmail.com>
2020-06-10 10:44:45 +0900

    third

--- app/src/main/java/com/example/activitytest/FirstActivity.java
+++ app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -46,8 +47,10 @@ public class FirstActivity extends AppCompatActivity {
                     //    Toast.LENGTH_SHORT).show();
                 //finish();
                 //Intent intent = new Intent(FirstActivity.this,SecondActivity.class);
-                Intent intent = new Intent("com.example.activitytest.ACTION_START");
-                intent.addCategory("com.example.activitytest.MY_CATEGORY");
+                //Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                //intent.addCategory("com.example.activitytest.MY_CATEGORY");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://www.baidu.com"));
                 startActivity(intent);
 
             }
